44f0798bb4bbed352377ecf326d286bf4427f91a
xiongxinwei <3293172751@qq.com>
2022-01-08 21:54:22 +0800

    配置

---
+++ code/16.最接近的三数之和.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=16 lang=cpp
+ *
+ * [16] 最接近的三数之和
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+
+    }
+};
+// @lc code=end
+
---
+++ code/37.解数独.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=37 lang=cpp
+ *
+ * [37] 解数独
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+
+    }
+};
+// @lc code=end
+
