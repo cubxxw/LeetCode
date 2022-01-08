7fd63b41e2405d781ec66a3782fd0d1f71228dcd
xiongxinwei <3293172751@qq.com>
2022-01-08 20:07:31 +0800

    init

---
+++ code/2039.网络空闲的时刻.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=2039 lang=cpp
+ *
+ * [2039] 网络空闲的时刻
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int networkBecomesIdle(vector<vector<int>>& edges, vector<int>& patience) {
+        
+    }
+};
+// @lc code=end
+
---
+++ code/3.无重复字符的最长子串.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=cpp
+ *
+ * [3] 无重复字符的最长子串
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+
+    }
+};
+// @lc code=end
+
