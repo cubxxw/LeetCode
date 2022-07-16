cdfc4d30925522e1d97c32071b2f75f37502cc31
xiongxinwei <86140903+3293172751@users.noreply.github.com>
2022-07-16 21:04:47 +0800

    Time: 0 ms (100.00%), Space: 6 MB (24.89%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 1)
+            return 1;
+        int min = 0;
+        int max = x;
+        while(max-min>1)
+        {
+            int m = (max+min)/2;
+            if(x/m<m)
+                max = m;
+            else
+                min = m;
+        }
+        return min;
+    }
+};
