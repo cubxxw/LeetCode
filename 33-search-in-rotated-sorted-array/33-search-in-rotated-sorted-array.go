8513152c7243d4e740379cf77185509e33c8b912
xiongxinwei <86140903+3293172751@users.noreply.github.com>
2022-07-20 18:13:42 +0800

    Time: 3 ms (69.36%), Space: 2.6 MB (71.68%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.go
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.go
@@ -1,42 +1,51 @@
-func search(nums []int, target int) int {
-    start, end := 0, len(nums)-1
-    for start+1 < end {
-        mid := (start+end)>>1
-        if nums[mid] > nums[len(nums)-1] {
-            start = mid
-        } else {
-            end = mid
-        }
-    }
-    var minIndex int
-    if nums[start] < nums[end] {
-        minIndex = start
-    } else {
-        minIndex = end
-    }
-    if nums[minIndex] == target {
-        return minIndex
-    }
-    if target > nums[len(nums)-1] {
-        start, end = 0, minIndex
-    } else {
-        start, end = minIndex, len(nums)-1
-    }
-    for start+1 < end {
-        mid := (start+end)>>1
+func find(nums []int, start int,end int,target int) int {
+    for start <= end {
+        mid := ((end - start) >> 1 ) + start;
+        
         if nums[mid] == target {
-            return mid
-        } else if nums[mid] > target {
-            end = mid
-        } else {
-            start = mid
+            return mid 
+        }else if nums[mid] > target {
+            end = mid -1
+        }else{
+            start = mid + 1
         }
     }
-    if nums[start] == target {
-        return start
-    }
-    if nums[end] == target {
-        return end
-    }
-    return -1
+    return -1   //找不到返回-1
 }
+
+func search(nums []int, target int) int {
+    len := len(nums)
+    if len == 0 {
+        return -1
+    }
+    if len == 1 {
+        if target == nums[0] {
+            return 0
+        }else{
+            return -1
+        }
+    }
+    
+    //找到旋转的位置
+    cutoff := -1
+    for i:=0; i < len - 1; i++ {
+        if nums[i] > nums[i+1] {
+            //smart
+            cutoff = i    //7所取到的位置
+            break
+        }
+    }
+    
+    //二分查找
+    left := find(nums,0,cutoff,target)
+    right := find(nums,cutoff+1,len-1,target)
+    
+    if left == -1 {
+        return right    
+    }else{
+        return left
+    }    
+}
+
+
+
