705b308d674b87a39c31e239700cebd9f169324d
xiongxinwei <86140903+3293172751@users.noreply.github.com>
2022-07-23 17:45:14 +0800

    Time: 0 ms (100.00%), Space: 43.4 MB (15.97%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -1,15 +1,23 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        pre, cur = None, head
-        while cur:
-            tmp = cur.next   #保存当前结点
-            cur.next = pre
-            pre = cur
-            cur = tmp    #当前结点是之前保存的结点  后移一位
-        return pre   #返回的是前一个结点        
-    
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+    ListNode pre = null;
+    ListNode cur = head;
+    while(cur != null){
+        ListNode tmp = cur.next;    //保存下一个结点
+        cur.next = pre;    //指向前一个结点
+        pre = cur;    //保存后续的结点
+        cur = tmp;   //当前结点是之前保存的哪个结点
+        }
+    return pre;
+    }
+}
