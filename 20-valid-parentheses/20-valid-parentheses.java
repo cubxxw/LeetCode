478468ef6182e3992ba99ab408da1c04837b8249
xiongxinwei <86140903+3293172751@users.noreply.github.com>
2022-07-28 13:03:52 +0800

    Time: 1 ms (99.63%), Space: 40.4 MB (90.08%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    //使用栈来解决当前的括号成对出现的问题
+    public boolean isValid(String str) {
+        Stack<Character> left = new Stack<>();
+        for (char c : str.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[')
+                left.push(c);
+            else // 字符 c 是右括号
+                if (!left.isEmpty() && leftOf(c) == left.peek())
+                    left.pop();
+                else
+                    // 和最近的左括号不匹配
+                    return false;
+        }
+        // 是否所有的左括号都被匹配了
+        return left.isEmpty();
+    }
+
+    char leftOf(char c) {
+        if (c == '}') return '{';
+        if (c == ')') return '(';
+        return '[';
+    }
+}
