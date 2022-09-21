003b8ac62d47e80838e02b8ab967c16d92b383ef
xiongxinwei <86140903+3293172751@users.noreply.github.com>
2022-09-21 16:45:25 +0800

    Time: 3 ms (68.58%), Space: 10.2 MB (6.73%) - LeetHub

---
+++ 100-same-tree/100-same-tree.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) {
+            return true;
+        } else if (p == nullptr || q == nullptr) {
+            return false;
+        }
+        queue <TreeNode*> queue1, queue2;
+        queue1.push(p);
+        queue2.push(q);
+        while (!queue1.empty() && !queue2.empty()) {
+            auto node1 = queue1.front();
+            queue1.pop();
+            auto node2 = queue2.front();
+            queue2.pop();
+            if (node1->val != node2->val) {
+                return false;
+            }
+            auto left1 = node1->left, right1 = node1->right, left2 = node2->left, right2 = node2->right;
+            if ((left1 == nullptr) ^ (left2 == nullptr)) {
+                return false;
+            }
+            if ((right1 == nullptr) ^ (right2 == nullptr)) {
+                return false;
+            }
+            if (left1 != nullptr) {
+                queue1.push(left1);
+            }
+            if (right1 != nullptr) {
+                queue1.push(right1);
+            }
+            if (left2 != nullptr) {
+                queue2.push(left2);
+            }
+            if (right2 != nullptr) {
+                queue2.push(right2);
+            }
+        }
+        return queue1.empty() && queue2.empty();
+    }
+};
+
